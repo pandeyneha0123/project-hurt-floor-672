f54ef42c82e7d7cf8f1ace8e0af3d844d339ef38
pandeyneha0123 <pandeyneha0123>
2023-01-18 17:50:42 +0530

    add

--- OneDrive/Desktop/SB101_Project/quarrelsome/src/com/models/Department.java
+++ OneDrive/Desktop/SB101_Project/quarrelsome/src/com/models/Department.java
@@ -1,5 +1,35 @@
 package com.models;
 
 public class Department {
+	
+	private String DepName;
+	private int DepNo;
+	
+	public Department() {
+		
+	}
+	
+	public Department(String Dname, int DNo, int DID) {
+		DepName = Dname;
+		DepNo = DNo;
+	}
+	
+	public String getDeptName() {
+		return DepName;
+	}
+	public void setDeptName(String Dname) {
+		DepName = Dname;
+	}
+	public int getDeptNo() {
+		return DepNo;
+	}
+	public void setDeptNo(int DNo) {
+		DepNo = DNo;
+	}
+	
+	@Override
+	public String toString() {
+		return "Department: [DeptName = "+DepName+", DeptNo = " + DepNo + "]";
+	}
 
 }
--- OneDrive/Desktop/SB101_Project/quarrelsome/src/com/models/Employee.java
+++ OneDrive/Desktop/SB101_Project/quarrelsome/src/com/models/Employee.java
@@ -1,5 +1,76 @@
 package com.models;
 
 public class Employee {
-
+	
+	private int EmpId;
+	private String EmpName;
+	private String EmpUserName;
+	private String EmpPassword;
+	private int Edid;
+	
+	public Employee() {
+		
+	}
+	
+public Employee(String ename, String username, String ePassword, int edid) {
+		
+		EmpName = ename;
+		EmpUserName = username;
+		EmpPassword = ePassword;
+		Edid = edid;
+	}
+	public Employee(int eid, String ename, String username, String ePassword, int edid) {
+			
+			EmpId = eid;
+			EmpName = ename;
+			EmpUserName = username;
+			EmpPassword = ePassword;
+			Edid = edid;
+		}
+	
+	public int getEmplId() {
+		return EmpId;
+	}
+	public void setEmplId(int eid) {
+		EmpId = eid;
+	}
+	
+	
+	public String getEmplName() {
+		return EmpName;
+	}
+	public void setEmplName(String ename) {
+		EmpName = ename;
+	}
+	
+	
+	public String getEmplUserName() {
+		return EmpUserName;
+	}
+	public void setEmplUserName(String username) {
+		EmpUserName = username;
+	}
+	
+	
+	public String getEmplPassword() {
+		return EmpPassword;
+	}
+	public void setEmplPassword(String ePassword) {
+		EmpPassword = ePassword;
+	}
+	
+	
+	public int getEdid() {
+		return Edid;
+	}
+	public void setEdid(int edid) {
+		Edid = edid;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Employee [EmplId=" + EmpId + ", EmplName=" + EmpName + ", EmplUserName=" + EmpUserName + ", EmplPassword=" + EmpPassword
+				+ ", Edid=" + Edid + "]";
+	}
 }
